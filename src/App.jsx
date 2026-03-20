7b7cf3a9f2f8e6dae8fb46bcd95e737bd62a3d3b
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-03-20 13:41:21 +0600

    practicing react

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
     <>
       <ShowName></ShowName>
       <ShowName></ShowName>
-      <ShowName></ShowName>
-      <ShowName></ShowName>
       <Daynamic></Daynamic>
       <UlList></UlList>
+      <Box name="Rimi" title="Full-Stack Dev"></Box>
+      <Box name="Ruba" title="Front-end Dev"></Box>
     </>
   );
 }
@@ -30,6 +30,19 @@ function UlList() {
       <li style={list}>bati</li>
       <li>khati</li>
     </ul>
+  );
+}
+const boxStyle = {
+  border: "1px solid white",
+  margin: "5px",
+  padding: "10px",
+};
+function Box(props) {
+  return (
+    <div style={boxStyle}>
+      <h2>Developer Name: {props.name}</h2>
+      <h2>Designation: {props.title}</h2>
+    </div>
   );
 }
 export default App;
